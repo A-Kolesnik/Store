c30db597184749709e089087501a8a1e6d7924e2
Andrey Kolesnik <kolesnik.dev@yandex.ru>
2023-06-30 19:43:05 +0300

    chore: Добавить схемы аутентификации
    Добавить схемы аутентификации в конфигурацию
    DRF в файле проекта settings.py. Доступные способы
    аутентификации: BasicAuthentication, TokenAuthentication

--- store/settings.py
+++ store/settings.py
@@ -83,6 +83,10 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
 SPECTACULAR_SETTINGS = {
